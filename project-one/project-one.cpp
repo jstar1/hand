eace1c10d3d943f3ffbbe3aac38ea9726d1606b2
jstar1 <jusmoore@pdx.edu>
2016-04-04 10:13:34 -0700

    modified:   project-one/project-one.cpp

--- project-one/project-one.cpp
+++ project-one/project-one.cpp
@@ -11,11 +11,21 @@ using namespace std;
 int main()
 {
 
-    cout << "Hello World" << endl;
-
-
-
-
-
+	int x;
+	int y;
+	
+	cout << "Hello World" << endl;
+	
+	cout << "Enter a number:" << endl;
+	cin >> x;
+	cout << "Enter another number:" << endl;
+	cin >> y;
+	
+	int z = x+y;
+	
+	cout << "This is " << x << " plus " << y << endl;
+	
+	cout << z << endl;
+	
     return 0;
 }
